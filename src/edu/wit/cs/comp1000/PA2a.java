e1dd485b1c9cdcdc928244c301d41bccfd54e2ec
Kat <lopezmanzanaresk@wit.edu>
2024-10-10 01:03:46 -0400

    made documentation

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,74 +1,71 @@
 package edu.wit.cs.comp1000;
 
 import java.util.Scanner;
-
-// TODO: document this class
+/**
+ * the purpose of this program is to take the sum of 5 whole numbers, positive or negative and calculate the sum and average of those numbers
+ * 
+ */
 public class PA2a {
 
-	// TODO: document this function
-	public static void main(String[] args) {
-		// TODO: write your code here
-		Scanner scanner = new Scanner(System.in);
-		System.out.printf("Enter five whole numbers: ");
+    public static void main(String[] args) {
+/**
+ * scanner input and prompts the user to input 5 whole numbers, positive or negative
+ * 
+ * 
+ * 
+ */
 
-//variables to track the count and sum of positive and negative numbers
-int[] numbers = new int [5];
-int countPos = 0, countNonPos = 0;
-int sumPos = 0, sumNonPos = 0, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter five whole numbers: ");
 
-for (int i = 0; i < 5; i++) {
+        int[] numbers = new int[5];
+        int countPos = 0, countNonPos = 0;
+        int sumPos = 0, sumNonPos = 0;
+        int totalSum = 0;
 
-	numbers[i] = scanner.nextInt();
+        /**
+		 * reading inputs and calculating
+		 * 
+		 */
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = sc.nextInt();
+            totalSum += numbers[i];
+            if (numbers[i] > 0) {
+                sumPos += numbers[i];
+                countPos++;
+            } else {
+                sumNonPos += numbers[i];
+                countNonPos++;
+            }
+        }
 
-	sum += numbers[i];
+        /**
+		 * calculating averages
+		 * 
+		 */
+        double avgPos = countPos > 0 ? (double) sumPos / countPos : 0.00;
+        double avgNonPos = countNonPos > 0 ? (double) sumNonPos / countNonPos : 0.00;
+        double avgTotal = (double) totalSum / 5;
 
-	if (numbers[i] > 0){
-		countPos++;
-		sumNonPos += numbers[i];
+        /**
+		 * plural differentiator
+		 * 
+		 */
+        String numPosText = countPos == 1 ? "number" : "numbers";
+        String numNonPosText = countNonPos == 1 ? "number" : "numbers";
 
-	}
+        /**
+		 * prints out the results of the sum of the non positive and non positive numbers, the averages, etc
+		 * 
+		 * 
+		 */
+        System.out.printf("The sum of the %d positive %s: %d%n", countPos, numPosText, sumPos);
+        System.out.printf("The sum of the %d non-positive %s: %d%n", countNonPos, numNonPosText, sumNonPos);
+        System.out.printf("The sum of the 5 numbers: %d%n", totalSum);
+        System.out.printf("The average of the %d positive %s: %.2f%n", countPos, numPosText, avgPos);
+        System.out.printf("The average of the %d non-positive %s: %.2f%n", countNonPos, numNonPosText, avgNonPos);
+        System.out.printf("The average of the 5 numbers: %.2f%n", avgTotal);
 
-else {
-
-countNonPos++;
-sumNonPos += numbers[i];
-
-
-}
-}
-
-
-
-double avgPos = (countPos > 0) ? (double) sumPos / countPos : 0.00; 
-double avgNonPos = (countNonPos > 0) ? (double) sumNonPos / countNonPos : 0.00; 
-double avg = (double) sum / 5; 
-
-
-        System.out.printf("The sum of the %d positive number%s: %d%n", countPos, (countPos == 1 ? "" : "s"), sumPos);
-        System.out.printf("The sum of the %d non-positive number%s: %d%n", countNonPos, (countNonPos == 1 ? "" : "s"), sumNonPos);
-        System.out.printf("The sum of the 5 numbers: %d%n", sum);
-        System.out.printf("The average of the %d positive number%s: %.2f%n", countPos, (countPos == 1 ? "" : "s"), avgPos);
-        System.out.printf("The average of the %d non-positive number%s: %.2f%n", countNonPos, (countNonPos == 1 ? "" : "s"), avgNonPos);
-        System.out.printf("The average of the 5 numbers: %.2f%n", avg);
-        
-
-        scanner.close();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	}
-
+        sc.close();
+    }
 }
