4b52668868d00c721906024d977ba0eb9b610597
Kat <lopezmanzanaresk@wit.edu>
2024-10-10 01:23:48 -0400

    made sure code passed test case, documenetation

--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,50 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
+/**
+ * the purpose of this program is to compute the roots of the quadratic formula and to calculate the discriminants
+ * 
+ * 
+ */
 public class PA2b {
 
-	// TODO: document this function
-	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a b c: ");
+        
+        double a = sc.nextDouble();
+    double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        
+        /**
+		 * Calculate discriminant (b^2 - 4ac)
+		 * 
+		 *  */ 
+        double discriminant = (b * b) - (4 * a * c);
+        
+        if (discriminant > 0) {
+            /**
+			 * 
+			 * two real roots
+			 */
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.printf("Roots: %.2f, %.2f%n", Math.min(root1, root2), Math.max(root1, root2));
+        } else if (discriminant == 0) {
+            /**
+			 * 
+			 * one real roots and one double root
+			 */
+            double root = -b / (2 * a);
+            System.out.printf("Root: %.2f%n", root);
+        } else {
+            /**
+			 * 
+			 * imaginary roots
+			 */
+        System.out.println("Roots: imaginary");
+        }
 
+        sc.close();
+    }
 }
